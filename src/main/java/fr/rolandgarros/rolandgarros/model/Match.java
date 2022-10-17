9f98fe5844d551a38e2b462396f29e60d07a6c8e
Louis Legrand <l.legrand@openium.fr>
2022-10-17 11:09:45 +0200

    Add time event

--- src/main/java/fr/rolandgarros/rolandgarros/model/Match.java
+++ src/main/java/fr/rolandgarros/rolandgarros/model/Match.java
@@ -4,13 +4,9 @@ import java.sql.Date;
 import java.util.Collections;
 import java.util.List;
 
-public abstract class Match {
+public abstract class Match extends TimeEvent {
 
     private final String genre;
-
-    private final Date startDate;
-
-    private Date endDate;
 
     private List<Integer> scoreOne;
 
@@ -19,45 +15,30 @@ public abstract class Match {
     private final Court court;
 
     public Match(String genre, Date startDate, Court court) {
+        super(startDate);
         this.genre = genre;
-        this.startDate = startDate;
         this.court = court;
     }
 
     public void endMatch(Date endDate, List<Integer> scoreOne, List<Integer> scoreTwo) {
-        this.endDate = endDate;
+        super.endTimeEvent(endDate);
         this.scoreOne = scoreOne;
         this.scoreTwo = scoreTwo;
-    }
-
-    public Boolean isMatchPassed() {
-        return endDate == null;
     }
 
     public String getGenre() {
         return genre;
     }
 
-    public Date getStartDate() {
-        return startDate;
-    }
-
-    public Date getEndDate() {
-        if (!isMatchPassed()) {
-            throw new RuntimeException("Match has not been passed yet");
-        }
-        return endDate;
-    }
-
     public List<Integer> getScoreOne() {
-        if (!isMatchPassed()) {
+        if (!isTimeEventPassed()) {
             throw new RuntimeException("Match has not been passed yet");
         }
         return Collections.unmodifiableList(scoreOne);
     }
 
     public List<Integer> getScoreTwo() {
-        if (!isMatchPassed()) {
+        if (!isTimeEventPassed()) {
             throw new RuntimeException("Match has not been passed yet");
         }
         return Collections.unmodifiableList(scoreTwo);
